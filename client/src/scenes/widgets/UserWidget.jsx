653116bf98630e27febc818b5fb17aa7609f6f6c
Deep-023 <deepanshuaggarwal023@gmail.com>
2023-07-30 18:09:33 +0530

    working on frontend made the login and signup page

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { WorkOutlineOutlined, ManageAccountsOutlined, EditOutlined, LocationCityOutlined } from '@mui/icons-material'
+import { Box, Typography, Divider, useTheme } from '@mui/material'
+import UserImage from 'components/UserImage'
+import WidgetWrapper from 'components/WidgetWrapper'
+import FlexBetween from 'components/FlexBetween'
+
+const UserWidget = () => {
+  return (
+    <div>UserWidget</div>
+  )
+}
+
+export default UserWidget
